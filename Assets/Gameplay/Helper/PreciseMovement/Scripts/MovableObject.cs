c08957524b4602e7a74242f8feec08120d2fe8a5
Moises Cumpa Torres <moises.cumpa@gmail.com>
2022-08-05 00:36:10 -0500

    Added basic movement from spawner to first target

--- Assets/Gameplay/Helper/PreciseMovement/Scripts/MovableObject.cs
+++ Assets/Gameplay/Helper/PreciseMovement/Scripts/MovableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace JMoisesCT.UnityMechanics.Helper.PreciseMovement
@@ -6,15 +7,25 @@ namespace JMoisesCT.UnityMechanics.Helper.PreciseMovement
     {
         private bool _isAlive;
         private Vector3 _speed;
+        private Vector2 _targetPosition;
+        private int _targetIndex;
+
+        public bool IsAlive => _isAlive;
 
         private void Awake()
         {
-            _isAlive = false;    
+            _isAlive = false;
         }
 
         public void Initialize()
         {
             _isAlive = true;
+        }
+
+        public void ResetObject()
+        {
+            _isAlive = false;
+            gameObject.SetActive(false);
         }
 
         public void SetSpeed(float speedX, float speedY)
@@ -27,12 +38,40 @@ namespace JMoisesCT.UnityMechanics.Helper.PreciseMovement
             transform.localPosition = new Vector3(posX, posY, 0f);
         }
 
-        private void Update()
+        public void SetTarget(Vector2 targetPosition, int targetIndex)
+        {
+            _targetPosition = targetPosition;
+            _targetIndex = targetIndex;
+        }
+
+        public void UpdateMove()
         {
             if (_isAlive)
             {
                 transform.localPosition += _speed * Time.deltaTime;
             }
+        }
+
+        public bool CheckIfTargetReached()
+        {
+            Vector3 nextPos = transform.localPosition + _speed * Time.deltaTime;
+            if (_speed.x != 0f)
+            {
+                if (_speed.x > 0f && nextPos.x > _targetPosition.x ||
+                    _speed.x < 0f && nextPos.x < _targetPosition.x)
+                {
+                    return true;
+                }
+            }
+            if (_speed.y != 0f)
+            {
+                if (_speed.y > 0f && nextPos.y > _targetPosition.y ||
+                    _speed.y < 0f && nextPos.y < _targetPosition.y)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- Assets/Gameplay/Helper/PreciseMovement/Scripts/SpawnerSystemBasic.cs
+++ Assets/Gameplay/Helper/PreciseMovement/Scripts/SpawnerSystemBasic.cs
@@ -18,19 +18,19 @@ namespace JMoisesCT.UnityMechanics.Helper.PreciseMovement
         [SerializeField] private float _speedMove;
         [SerializeField] [Tooltip("In milliseconds")] private float _spawnRate;
 
-        private List<Vector3> _targetsPosition;
+        private List<Transform> _targets;
         private List<MovableObject> _balls;
         private float _timer;
 
         private void Awake()
         {
-            _targetsPosition = new List<Vector3>();
+            _targets = new List<Transform>();
 
             Transform[] targets = _targetsContainer.GetComponentsInChildren<Transform>();
             // Ignore the position 0 Transform (it's the parent)
-            for (int i = 1; i < targets.Length; i++)
+            for (int i = 1; i < targets.Length; ++i)
             {
-                _targetsPosition.Add(targets[i].localPosition);
+                _targets.Add(targets[i]);
             }
 
             _balls = new List<MovableObject>();
@@ -47,16 +47,29 @@ namespace JMoisesCT.UnityMechanics.Helper.PreciseMovement
                 {
                     MovableObject ball = Instantiate(_ball, _ballsContainer).GetComponent<MovableObject>();
                     // Let's calculate the direction to the first target.
-                    Vector2 direction = _targetsPosition[0] - _spawner.localPosition;
+                    Vector2 direction = _targets[0].localPosition - _spawner.localPosition;
                     Vector2 speedToDirection = (direction / direction.magnitude) * _speedMove;
                     ball.SetPosition(_spawner.localPosition.x, _spawner.localPosition.y);
                     ball.SetSpeed(speedToDirection.x, speedToDirection.y);
+                    ball.SetTarget(_targets[0].localPosition, 0);
                     ball.Initialize();
                     _balls.Add(ball);
                     _timer = _spawnRate * 0.001f;
                 }
             }
-        }
 
+            for (int i = 0; i < _balls.Count; ++i)
+            {
+                // Check if any ball has reached the target.
+                if (_balls[i].CheckIfTargetReached())
+                {
+                    _balls[i].ResetObject();
+                }
+                if (_balls[i].IsAlive)
+                {
+                    _balls[i].UpdateMove();
+                }
+            }
+        }
     }
 }

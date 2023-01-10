304d710dbb5472b2a00687ffd3ab4ee9703fe0a1
Moises Cumpa Torres <moises.cumpa@gmail.com>
2023-01-10 01:27:16 -0500

    Testing GetPixels (Texture must have read/write property enabled)

--- Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
+++ Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
@@ -4,10 +4,13 @@ namespace JMoisesCT.UnityMechanics.Tools.TilemapExtrusion
 {
     public class TileExtruder : MonoBehaviour
     {
+        [SerializeField] private Texture2D _texture;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            Color[] pixels = _texture.GetPixels();
+            Debug.Log($"{pixels}");
         }
 
         // Update is called once per frame

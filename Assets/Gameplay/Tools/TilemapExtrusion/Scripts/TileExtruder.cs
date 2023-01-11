04be364bff45456ecedba2d94212e87bc5a10595
Moises Cumpa Torres <moises.cumpa@gmail.com>
2023-01-11 00:49:10 -0500

    Testing setpixels and getpixels methods from Texture, testing new sprite creation

--- Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
+++ Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
@@ -6,17 +6,42 @@ namespace JMoisesCT.UnityMechanics.Tools.TilemapExtrusion
     {
         [SerializeField] private Texture2D _texture;
 
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private SpriteRenderer _renderer;
+
         // Start is called before the first frame update
         void Start()
         {
-            Color[] pixels = _texture.GetPixels();
-            Debug.Log($"{pixels}");
+            
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Color[] pixels = _texture.GetPixels();
+                Debug.Log($"{pixels}");
 
+                int width = _texture.width;
+                int height = _texture.height;
+
+                //Texture2D texture = new Texture2D(_texture.width + 2, _texture.height + 2);
+                Texture2D texture = new Texture2D(width, height);
+                
+                for (int y = 0; y < texture.height; y++)
+                {
+                    for (int x = 0; x < texture.width; x++)
+                    {
+                        texture.SetPixel(x, y, pixels[x + y * width]);
+                    }
+                }
+
+                texture.Apply();
+                _sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, width, height), new Vector2(0.5f, 0.5f), 100.0f);
+
+                _renderer.sprite = _sprite;
+            }
         }
     }
 }

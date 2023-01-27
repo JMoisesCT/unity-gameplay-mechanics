a28068ba55ba3a7f5634f2ef3e78c35823da5dfc
Moises Cumpa Torres <moises.cumpa@gmail.com>
2023-01-27 02:17:08 -0500

    TileExtruder now extends a pixel for each side of the texture

--- Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
+++ Assets/Gameplay/Tools/TilemapExtrusion/Scripts/TileExtruder.cs
@@ -12,7 +12,7 @@ namespace JMoisesCT.UnityMechanics.Tools.TilemapExtrusion
         // Start is called before the first frame update
         void Start()
         {
-            
+
         }
 
         // Update is called once per frame
@@ -23,22 +23,37 @@ namespace JMoisesCT.UnityMechanics.Tools.TilemapExtrusion
                 Color[] pixels = _texture.GetPixels();
                 Debug.Log($"{pixels}");
 
-                int width = _texture.width;
-                int height = _texture.height;
+                int originalWidth = _texture.width;
+                int originalHeight = _texture.height;
+                int newWidth = originalWidth + 2;
+                int newHeight = originalHeight + 2;
 
                 //Texture2D texture = new Texture2D(_texture.width + 2, _texture.height + 2);
-                Texture2D texture = new Texture2D(width, height);
-                
-                for (int y = 0; y < texture.height; y++)
+                Texture2D texture = new Texture2D(newWidth, newHeight);
+
+                for (int y = 0; y < originalHeight; y++)
                 {
-                    for (int x = 0; x < texture.width; x++)
+                    for (int x = 0; x < originalWidth; x++)
                     {
-                        texture.SetPixel(x, y, pixels[x + y * width]);
+                        int copyY = (y == 0) ? 0 : (y == originalHeight - 1) ? newHeight - 1 : -1;
+                        if (copyY != -1)
+                        {
+                            texture.SetPixel(x + 1, copyY, pixels[x + y * originalWidth]);
+                        }
+                        int copyX = (x == 0) ? 0 : (x == originalWidth - 1) ? newWidth - 1 : -1;
+                        if (copyX != -1)
+                        {
+                            texture.SetPixel(copyX, y + 1, pixels[x + y * originalWidth]);
+                        }
+                        //if ( x == 0 || x == _texture.width || )
+                        // This code copies the texture in the center of the new texture.
+                        texture.SetPixel(x + 1, y + 1, pixels[x + y * originalWidth]);
                     }
                 }
 
+                texture.filterMode = _texture.filterMode;
                 texture.Apply();
-                _sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, width, height), new Vector2(0.5f, 0.5f), 100.0f);
+                _sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, newWidth, newHeight), new Vector2(0.5f, 0.5f), 100.0f);
 
                 _renderer.sprite = _sprite;
             }
